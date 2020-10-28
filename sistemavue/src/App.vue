553bf1cb88691156d449932d9b3ab633c4c1bb67
Anthony Rodriguez <anthony81199@gmail.com>
2020-10-28 01:06:46 -0400

    Añadiendo:
    - Proyecto Frontend Vue base

---
+++ sistemavue/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-content height="100%">
+      <div>
+        <Navbar :title="sectionName" />
+        <Sidebar @setSectionName="setSectionName" />
+        <router-view class="pl-14"></router-view>
+      </div>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar'
+import Navbar from '@/components/Navbar'
+
+export default {
+  name: 'App',
+  components: {
+    Sidebar,
+    Navbar,
+  },
+  data: () => ({
+    sectionName: 'Home',
+  }),
+  methods: {
+    setSectionName(newValue) {
+      this.sectionName = newValue
+    },
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ sistemavue/src/components/Sidebar.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-navigation-drawer class="navigation-drawer" permanent expand-on-hover>
+    <v-list nav dense>
+      <v-list-item-group
+        v-model="group"
+        active-class="deep-purple--text text--accent-2"
+      >
+        <v-list-item
+          v-for="item in sidebarItems"
+          :key="item.title"
+          link
+          @click="goRoute(item.route)"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  data: () => ({
+    group: null,
+    sidebarItems: [
+      { title: 'Home', icon: 'mdi-home', route: 'Home' },
+      { title: 'Account', icon: 'mdi-account', route: 'Account' },
+    ],
+  }),
+  methods: {
+    goRoute(route) {
+      this.$router.push({ name: `${route}` })
+      this.setCurrentSectionName(route)
+    },
+    setCurrentSectionName(sectionName) {
+      this.$emit('setSectionName', sectionName)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.navigation-drawer {
+  position: fixed;
+}
+</style>
