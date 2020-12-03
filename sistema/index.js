50907a9612c650672dcbc6f1979f910296a78ca1
Anthony Rodriguez <anthony81199@gmail.com>
2020-12-03 16:23:10 -0400

    se agrega data table con información estática en categorías

--- sistema/index.js
+++ sistema/index.js
@@ -1,17 +1,17 @@
-import express from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
-import path from 'path'
-import mongoose from 'mongoose'
-import router from './routes'
-import dotenv from 'dotenv'
-dotenv.config()
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import path from "path";
+import mongoose from "mongoose";
+import router from "./routes";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
-app.use(morgan('dev'))
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.join(__dirname, 'public')))
+const app = express();
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 // db connection
 
@@ -24,17 +24,20 @@ app.use(express.static(path.join(__dirname, 'public')))
 //         err => console.log(err)
 //     )
 
-console.log('Conecting to database...')
-mongoose.connect(process.env.DB_URI, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
+console.log("Conecting to database...");
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
     useCreateIndex: true,
-}).then(() => console.log('dbMEVN connection successful')).catch((e) => console.log(e))
+  })
+  .then(() => console.log("dbMEVN connection successful"))
+  .catch((e) => console.log(e));
 
-app.use('/api',router)
-app.set('port', process.env.PORT || 3000)
+app.use("/api", router);
+app.set("port", process.env.PORT || 3001);
 
-app.listen(app.get('port'), () => {
-    console.log(`Server running in port ${app.get('port')}`)
-    console.log(path.join(__dirname, 'public'))
-})
+app.listen(app.get("port"), () => {
+  console.log(`Server running in port ${app.get("port")}`);
+  console.log(path.join(__dirname, "public"));
+});
